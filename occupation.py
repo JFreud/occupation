5ea7314ac5f24132c0a5b778947829de1fe24691
JFreud <jrm.frdnbrg@gmail.com>
2017-09-15 13:49:32 -0400

    doesnt seem to be working

--- occupation.py
+++ occupation.py
@@ -1,19 +1,44 @@
 import csv
+import random
 
 
-
-def makeDict(file):
+def make_dict(file):
     occD = {}
-    with open(file, 'r') as occ_csv:
-        reader = csv.DictReader(occ_csv)
-        ## for occ in reader:
-        ##     #print(occ)
-        ##     print(occ[1])
-        ##     occD[occ[0]] = float(occ[1])
-        for occ in reader:
-            occD.update(occ)
+    input_file = open(file)
+    reader = csv.DictReader(input_file)
+    for occ in reader:
+        #print occ['Job Class']
+        #print occ['Percentage']
+        occD[occ['Job Class']] = float(occ['Percentage'])
+    occD.pop('Total')
     return occD
 
+def pick_random(occD, total):
+    chosen = random.uniform(0, total)
+    for key in occD:
+        if chosen <= 0:
+            return key + ", " + str(occD[key])
+        else:
+            chosen -= occD[key]
+    return "That's not right"
 
-print(makeDict("occupations.csv"))
+
+#print(make_dict("occupations.csv"))
+occs = make_dict("occupations.csv")
+print pick_random(occs, 99.8)
+
+testerD = {}
+top = 10000000.0
+
+for i in range (1, int(top)):
+    this = pick_random(occs, 99.8)
+   # print this
+    try:
+        testerD[this] += 1
+    except:
+        testerD[this] = 1
+    #print testerD
+for key in testerD:
+    testerD[key] = testerD[key] / top * 100
+print testerD
     
